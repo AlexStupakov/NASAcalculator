5e30f59e77aaee4782e11ae98e169fba033b455a
Oleksandr Stupakov <stepan.alex@gmail.com>
2020-07-21 14:11:04 +0300

    added tests descriptions

--- spec/args_parser_spec.rb
+++ spec/args_parser_spec.rb
@@ -3,55 +3,57 @@ require_relative '../args_parser'
 
 RSpec.describe ArgsParser do
   describe 'method get_ship_weight_integer' do
-    it do
+    it 'should respond to get_ship_weight_integer' do
       expect(ArgsParser).to respond_to(:get_ship_weight_integer)
     end
 
-    it do
+    it 'should return correct result' do
       expect(ArgsParser.get_ship_weight_integer('28801, [[:land, 9.807]]')).to eq 28801
     end
 
-    it do
-      expect(ArgsParser.get_ship_weight_integer(', [[:land, 9.807]]')).to eq 0
-    end
+    describe 'incorrect arguments return 0' do
+      it do
+        expect(ArgsParser.get_ship_weight_integer(', [[:land, 9.807]]')).to eq 0
+      end
 
-    it do
-      expect(ArgsParser.get_ship_weight_integer('[[:land, 9.807]]')).to eq 0
-    end
+      it do
+        expect(ArgsParser.get_ship_weight_integer('[[:land, 9.807]]')).to eq 0
+      end
 
-    it do
-      expect(ArgsParser.get_ship_weight_integer('bla')).to eq 0
+      it do
+        expect(ArgsParser.get_ship_weight_integer('bla')).to eq 0
+      end
     end
   end
 
-  describe 'method calculate_whole_way_fuel' do
-    it do
+  describe 'method calculate_whole_way_fuel parse route args from string' do
+    it 'should respond to get_flight_route_array' do
       expect(ArgsParser).to respond_to(:get_flight_route_array)
     end
 
-    it do
-      expect(ArgsParser.get_flight_route_array('28801, [[:land, 9.807]]')).to eq [[:land, 9.807]]
-    end
-
-    it do
+    it 'without a first argument' do
       expect(ArgsParser.get_flight_route_array('[[:land, 9.807]]')).to eq [[:land, 9.807]]
     end
 
-    it do
+    it 'with many arguments' do
       expect(ArgsParser.get_flight_route_array('1, 2, 3, [:land, 9.807]')).to eq [[:land, 9.807]]
     end
 
-    it do
-      expect(ArgsParser.get_flight_route_array('28801, [[:launch, 9.807]]')).to eq [[:launch, 9.807]]
-    end
-
-    it do
+    it 'should pass only :launch or :land' do
       expect(ArgsParser.get_flight_route_array('28801, [[:bla, 9.807]]')).to eq []
+      expect(ArgsParser.get_flight_route_array('28801, [[:launch, 9.807]]')).to eq [[:launch, 9.807]]
+      expect(ArgsParser.get_flight_route_array('28801, [[:land, 9.807]]')).to eq [[:land, 9.807]]
     end
 
-    it do
-      expect(ArgsParser.get_flight_route_array('28801, [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 9.807]]'))
-        .to eq [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 9.807]]
+    describe 'correct args' do
+      it 'simple route' do
+        expect(ArgsParser.get_flight_route_array('28801, [[:land, 9.807]]')).to eq [[:land, 9.807]]
+      end
+
+      it 'long route' do
+        expect(ArgsParser.get_flight_route_array('28801, [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 9.807]]'))
+          .to eq [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 9.807]]
+      end
     end
   end
 end
--- spec/fuel_calculation_spec.rb
+++ spec/fuel_calculation_spec.rb
@@ -3,21 +3,21 @@ require_relative '../fuel_calculation'
 RSpec.describe FuelCalculation do
 
   describe 'method calculate_one_action_fuel' do
-    it do
+    it 'should respond to calculate_one_action_fuel' do
       expect(FuelCalculation).to respond_to(:calculate_one_action_fuel)
     end
 
-    it do
+    it 'should return correct result' do
       expect(FuelCalculation.calculate_one_action_fuel(28801, 9.807, :land)).to eq 13447
     end
   end
 
   describe 'method calculate_whole_way_fuel' do
-    it do
+    it 'should respond to calculate_whole_way_fuel' do
       expect(FuelCalculation).to respond_to(:calculate_whole_way_fuel)
     end
 
-    it do
+    it 'should return correct result' do
       expect(FuelCalculation.calculate_whole_way_fuel(28801, [[:land, 9.807]])).to eq 13447
     end
   end
